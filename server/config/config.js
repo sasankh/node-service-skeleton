e2ec9d7951f48aad67f1b5caa4861472d8aeafba
Sasankh BC <bcsasankh@gmail.com>
2017-03-04 12:41:49 -0800

    configured the log initialization

--- server/config/config.js
+++ server/config/config.js
@@ -1,21 +1,22 @@
 'use strict';
 
-// configuration for local environment
-// the configuration will be repeated for all envs
-// local, test, dev, stage and production
-
-exports.application =  process.env.APPLICATION || 'placeholder-for-application-name';
+exports.application =  process.env.APPLICATION;
 
 exports.app = {
   port: process.env.PORT || 3000,
-  cryptoKey: process.env.CRYPTOKEY || 'placeholder-for-cryptokey',
-  env: process.env.ENVIRONMENT || 'placeholder-for-deployment-environment'
+  cryptoKey: process.env.CRYPTOKEY,
+  env: process.env.ENVIRONMENT
 };
 
 exports.mysql = {
-  host: process.env.MYSQL_HOST || 'placeholder-for-mysql-host',
-  user: process.env.MYSQL_USER || 'placeholder-for-mysql-user',
-  password: process.env.MYSQL_PASS || 'placeholder-for-mysql-password',
-  database: process.env.MYSQL_DB || 'placeholder-for-mysql-database',
-  port: process.env.MYSQL_PORT || 'placeholder-for-mysql-port'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
+  port: process.env.MYSQL_PORT
+};
+
+exports.log = {
+  log_level: process.env.MAIN_LOG_LEVEL || 'ALL',
+  log_path: process.env.LOG_PATH || '.'
 };
--- server/init/modules/logger.js
+++ server/init/modules/logger.js
@@ -1,13 +1,159 @@
 'use strict';
 
 var log4js = require('log4js');
+var fs = require('fs');
+var async = require('async');
 
-// logger
-log4js.configure('./server/config/log.json');
+var config = require(__base + '/server/config/config');
+
+var logFolderName = 'logs';
+var logLeveList = [
+  'ALL',
+  'TRACE',
+  'DEBUG',
+  'INFO',
+  'WARN',
+  'ERROR',
+  'FATAL',
+  'OFF'
+];
+
+switch (config.app.env){
+  case 'production':
+  logFolderName = 'logs';
+  break;
+
+  case 'stage':
+  logFolderName = 'logs-stage';
+  break;
+
+  case 'dev':
+  logFolderName = 'logs-dev';
+  break;
+
+  default:
+  logFolderName = 'logs';
+}
+
+var main;
+var healthcheck;
+var auth;
+var http;
+
+var logDirs = [
+  "/" + logFolderName,
+  "/" + logFolderName + '/main',
+  "/" + logFolderName + '/health',
+  "/" + logFolderName + '/auth',
+  "/" + logFolderName + '/access'
+];
+
+async.map(logDirs, function(path, callback){
+
+  try {
+
+    fs.mkdirSync(config.log.log_path + path);
+    callback(null, true);
+
+  } catch(e) {
+
+    if ( e.code === 'EEXIST' ) {
+      callback(null, true);
+    }else{
+      callback(e, false);
+    }
+
+  }
+
+},function(err, results){
+
+  if(err){
+    throw err;
+  }else{
+    initializeLog4js();
+  }
+
+});
+
+function initializeLog4js(){
+
+  log4js.configure(
+    {
+      'appenders': [
+        {
+          "type": "console"
+        },
+        {
+          "type": "clustered",
+          "appenders": [
+            {
+              "type": "dateFile",
+              "category": "http",
+              "filename": config.log.log_path + "/" + logFolderName + "/access/access",
+              "pattern": "-yyyy-MM-dd.log",
+              "alwaysIncludePattern": true,
+              "layout": {
+                "type": "pattern",
+                "pattern": "[%d{ISO8601}] [%p] - %c - %m%n"
+              }
+            },
+            {
+              "type": "dateFile",
+              "filename": config.log.log_path + "/" + logFolderName + "/main/main",
+              "category": "application",
+              "pattern": "-yyyy-MM-dd.log",
+              "alwaysIncludePattern": true,
+              "layout": {
+                "type": "pattern",
+                "pattern": "[%d{ISO8601}] [%p] - %c - %m%n"
+              }
+            },
+            {
+              "type": "dateFile",
+              "filename": config.log.log_path + "/" + logFolderName + "/health/health",
+              "category": "health-check",
+              "pattern": "-yyyy-MM-dd.log",
+              "alwaysIncludePattern": true,
+              "layout": {
+                "type": "pattern",
+                "pattern": "[%d{ISO8601}] [%p] - %c - %m"
+              }
+            },
+            {
+              "type": "dateFile",
+              "filename": config.log.log_path + "/" + logFolderName + "/auth/auth",
+              "category": "auth",
+              "pattern": "-yyyy-MM-dd.log",
+              "alwaysIncludePattern": true,
+              "layout": {
+                "type": "pattern",
+                "pattern": "%d{yyyy-MM-dd hh:mm:ss,SSS} %m"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  );
+
+  main = log4js.getLogger('application');
+  healthcheck = log4js.getLogger('health-check');
+  auth = log4js.getLogger('auth');
+  http = log4js.connectLogger(log4js.getLogger('http'), { level: 'auto' });
+
+  if(logLeveList.indexOf(config.log.log_level) > -1){
+    main.info('Log level set to: ' + config.log.log_level);
+    main.setLevel(config.log.log_level);
+  }else{
+    main.info('Log level set to: ALL (default)');
+    main.setLevel('ALL');
+  }
+
+}
 
 module.exports = {
-  main: log4js.getLogger('application'),
-  healthcheck: log4js.getLogger('health-check'),
-  auth: log4js.getLogger('authentication-authorization'),
-  http: log4js.getLogger('http')
+  main : main,
+  healthcheck : healthcheck,
+  auth : auth,
+  http : http
 };
